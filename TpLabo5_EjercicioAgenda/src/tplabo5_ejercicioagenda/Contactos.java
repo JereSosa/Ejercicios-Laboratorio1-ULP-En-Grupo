3c54fca8f35584ab105cbc21048c6a8358f260f2
jontive21 <joseeontiveros@sanluis.edu.ar>
2024-09-04 20:47:31 -0300

    subiendo

--- TpLabo5_EjercicioAgenda/src/tplabo5_ejercicioagenda/Contactos.java
+++ TpLabo5_EjercicioAgenda/src/tplabo5_ejercicioagenda/Contactos.java
@@ -8,8 +8,69 @@ package tplabo5_ejercicioagenda;
  *
  * @author Esquina del Vidrio
  */
+
+
 public class Contactos {
- 
+    private String DNI;
+    private String nombre;
+    private String apellido;
+    private String ciudad;
+    private String direccion;
+
+    public Contactos(String DNI, String nombre, String apellido, String ciudad, String direccion) {
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto [DNI=" + DNI + ", nombre=" + nombre + ", apellido=" + apellido + 
+               ", ciudad=" + ciudad + ", direccion=" + direccion + "]";
+    }
+} 
    
  
-}
+
--- TpLabo5_EjercicioAgenda/src/tplabo5_ejercicioagenda/Directorio.java
+++ TpLabo5_EjercicioAgenda/src/tplabo5_ejercicioagenda/Directorio.java
@@ -4,13 +4,54 @@
  */
 package tplabo5_ejercicioagenda;
 
+
 import java.util.Set;
 import java.util.TreeMap;
 
+
+import java.util.*;
+
+public class Directorio {
+    private TreeMap<Long, Contactos> contactos;
+
+    public Directorio() {
+        this.contactos = new TreeMap<>();
+    }
+
+    public void agregarContacto(Long telefono, Contactos contacto) {
+        contactos.put(telefono, contacto);
+    }
+
+    public Contactos buscarContacto(Long telefono) {
+        return contactos.get(telefono);
+    }
+
+    public Set<Long> buscarTelefono(String apellido) {
+        Set<Long> telefonos = new TreeSet<>();
+        for (Map.Entry<Long, Contactos> entry : contactos.entrySet()) {
+            if (entry.getValue().getApellido().equals(apellido)) {
+                telefonos.add(entry.getKey());
+            }
+        }
+        return telefonos;
+    }
+
+    public ArrayList<Contactos> buscarContactos(String ciudad) {
+        ArrayList<Contactos> listaContactos = new ArrayList<>();
+        for (Contactos contacto : contactos.values()) {
+            if (contacto.getCiudad().equals(ciudad)) {
+                listaContactos.add(contacto);
+            }
+        }
+        return listaContactos;
+    }
+
+    public boolean borrarContacto(Long telefono) {
+        return contactos.remove(telefono) != null;
+    }
+}
 /**
  *
  * @author Esquina del Vidrio
  */
-public class Directorio {
-    
-}
+
